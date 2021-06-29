a2bb278fff2b7730c56cbd18bdaa6e6a782b135e
timahtirski <timurpls@gmail.com>
2021-06-29 00:25:26 -0400

    added console

---
+++ myconsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myconsole
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
